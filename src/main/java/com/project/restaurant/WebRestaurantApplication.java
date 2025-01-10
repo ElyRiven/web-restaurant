28f2c44cfb9bb7bb91d6b23cce16e13baaad1e7d
Elizabeth Tamayo <byrdanto@gmail.com>
2025-01-10 16:17:46 -0500

    Primer commit del proyecto

---
+++ src/main/java/com/project/restaurant/WebRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.project.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRestaurantApplication.class, args);
+	}
+
+}
